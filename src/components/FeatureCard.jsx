04fdb2eb58b466adc475f056e3b95a57f20b695f
woohyuk0428 <whkang03@gmail.com>
2026-01-22 16:54:57 +0900

    #1 feat: FeatureCard 내 css값 추가 및 ux 화면 수정

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,9 +1,11 @@
 import "../styles/home.css";
 
-export default function FeatureCard({ tone, title, desc }) {
+export default function FeatureCard({ tone, title, desc, icon: Icon }) {
   return (
     <div className={`feature-card tone-${tone}`}>
-      <div className="feature-icon" aria-hidden />
+      <div className="feature-icon">
+        <Icon className="feature-icon-svg" size={28} />
+      </div>
       <div className="feature-title">{title}</div>
       <div className="feature-desc">{desc}</div>
     </div>
